349b976c6bf612970098f526d3341071473486c5
leifufeng <1500447979@qq.com>
2017-08-23 21:46:55 +0800

    Add files via upload

---
+++ git.js
@@ -0,0 +1,74 @@
+git.js
+
+git与github安装、配置、pull、push
+http://www.cnblogs.com/qiaoconglovelife/p/5506643.html
+
+
+GIT-查看config配置信息
+http://www.cnblogs.com/merray/p/6006411.html
+
+
+git 解决fatal: Not a git repository
+http://www.cnblogs.com/kristain/articles/3840000.html
+
+
+1.5 起步 - 初次运行 Git 前的配置
+https://git-scm.com/book/zh/v1/%E8%B5%B7%E6%AD%A5-%E5%88%9D%E6%AC%A1%E8%BF%90%E8%A1%8C-Git-%E5%89%8D%E7%9A%84%E9%85%8D%E7%BD%AE
+
+常用 Git 命令清单
+http://www.ruanyifeng.com/blog/2015/12/git-cheat-sheet.html
+
+git push
+http://www.yiibai.com/git/git_push.html
+
+ Git push
+ http://www.cnblogs.com/qianqiannian/p/6008140.html
+
+ git push.default设置 
+ http://blog.csdn.net/daijingxin/article/details/51326715
+
+ git与github安装、配置、pull、push
+ http://www.cnblogs.com/qiaoconglovelife/p/5506643.html
+
+ git pull
+ http://www.yiibai.com/git/git_pull.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
